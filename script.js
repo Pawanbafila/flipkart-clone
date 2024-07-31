978f947209b8bf4a89b7ed8eee6263bfffb7c69a
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-07-31 11:44:17 +0530

    add some new nav animation

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
         document.querySelector('.header-two-hide').style.display = 'block';
     }
 
-    function offbar() {
-        document.querySelector('.header-two-hide').style.display = 'none';
+ function offbar() {
+        const menu = document.querySelector('.header-two-hide');
+        menu.style.animation = 'slideroff 1s ease-in-out';
+        setTimeout(() => {
+           menu.style.display = 'none';
+        }, 800); // Match the duration of the animation
     }
-
