3006aae4bb35fb24c46134e0bb56d8e13aea493c
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-07-31 11:48:31 +0530

    remove nav animation probelms

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
     function onbar() {
-        document.querySelector('.header-two-hide').style.display = 'block';
+        const menu = document.querySelector('.header-two-hide');
+        menu.style.animation = 'slideron 1s ease-in-out';
+        setTimeout(() => {
+           menu.style.display = 'block';
+        }, 0.1); // Match the duration of the animation
+
     }
 
  function offbar() {
